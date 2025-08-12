1adf34b144592433bf07fe7732bbfc411071d81e
Adilet <127068760+AdiletZalgaraev@users.noreply.github.com>
2025-08-12 21:48:47 +0500

    Add files via upload

---
+++ exam/java-main.js
@@ -0,0 +1,37 @@
+
+fetch("https://fakestoreapi.in/api/products/category?offset=4&type=mobile")
+.then (response => response.json())
+.then (data => {
+    console.log(data);
+    let result = data.products
+    let container = document.getElementById('product-place')
+    result.forEach(element => {
+        console.log(element.title);
+        console.log(element.image);
+        console.log(element.price);
+
+        let card = document.createElement('div')
+        card.classname = "product-div";
+
+        let title = document.createElement("h5");
+        title.textContent = element.title ;
+        
+        let image = document.createElement("img");
+        image.src = element.image;
+
+        let price = document.createElement("h4");
+        price.textContent = element.price + "$";
+
+        let add = document.createElement("button");
+        add.textContent = "To box";
+        add.className = "add-to-cart"
+
+
+        card.appendChild(add);
+        card.appendChild(image);
+        card.appendChild(title);
+        card.appendChild(price);
+
+        container.appendChild(card)
+    });
+})
